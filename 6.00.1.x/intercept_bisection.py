11b6c5f19c38a3c8139dda6aa160e1183a6f2576
prasannabe2004 <prasannabe2004@gmail.com>
2014-09-08 19:21:21 -0500

    cleaning

--- 6.00.1.x/intercept_bisection.py
+++ 6.00.1.x/intercept_bisection.py
@@ -4,25 +4,30 @@
 # Use bisection search. 
 # In other words, find 'a' such that f(a) = 0 to some certainty 'epsilon'.
 
-low = 0
-high = 100
-epsilon = 0.0001
-guess = 0
-mid = (low+high)/2.0
-
 def f(x):
     return -5+2.77*x
+    
+def g(x):
+    return -5+0.08*x
 
-print abs(f(mid))
-
-while (abs(f(mid)) > epsilon):
-    print guess,mid
-    guess = guess +1
-    if f(mid) > 0:
-        high = mid
-    elif f(mid) < 0:
-        low = mid;
+def intercept_bisection(func,epsilon):
+    low = 0
+    high = 100
+    epsilon = 0.0001
+    guess = 0
     mid = (low+high)/2.0
     
-print "a = ",mid, " with ",guess, " guesses ", 5/2.77
+    while (abs(f(mid)) > epsilon):
+        print guess,mid
+        guess = guess +1
+        if f(mid) > 0:
+            high = mid
+        elif f(mid) < 0:
+            low = mid;
+        mid = (low+high)/2.0
+    return mid,guess
+    
+a,guess = intercept_bisection(g,0.1)
+
+print "a = ",a, " with ",guess, " guesses "
 
