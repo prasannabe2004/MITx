374bd369a3bf30b3a8ce0c8fe492aee68437063e
prasannabe2004 <prasannabe2004@gmail.com>
2014-09-18 16:28:42 -0500

    Fixing compilation issue with pycharm on Lecture 8 Problem Set 4.

--- 6.00.1.x/ProblemSet4/ps4a.py
+++ 6.00.1.x/ProblemSet4/ps4a.py
@@ -88,7 +88,7 @@ def displayHand(hand):
     Displays the letters currently in the hand.
 
     For example:
-    >>> displayHand({'a':1, 'x':2, 'l':3, 'e':1})
+    displayHand({'a':1, 'x':2, 'l':3, 'e':1})
     Should print out something like:
        a x x l l l e
     The order of the letters is unimportant.
