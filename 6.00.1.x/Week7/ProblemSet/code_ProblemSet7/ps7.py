8a8bb53c27d9a5a83ccc4a9cd4cb2188bf30b290
prasannabe2004 <prasannabe2004@gmail.com>
2014-10-20 14:52:58 -0500

    Problem set 7  2 steps completed.

--- 6.00.1.x/Week7/ProblemSet/code_ProblemSet7/ps7.py
+++ 6.00.1.x/Week7/ProblemSet/code_ProblemSet7/ps7.py
@@ -47,6 +47,24 @@ def process(url):
 # Problem 1
 
 # TODO: NewsStory
+class NewsStory(object):
+    def __init__(self, guid, title, subject, summary, link):
+        self.guid = guid
+        self.title = title
+        self.subject = subject
+        self.summary = summary
+        self.link = link
+
+    def getGuid(self):
+        return self.guid
+    def getTitle(self):
+        return self.title
+    def getSubject(self):
+        return self.subject
+    def getSummary(self):
+        return self.summary
+    def getLink(self):
+        return self.link
 
 #======================
 # Part 2
@@ -64,12 +82,34 @@ class Trigger(object):
 # Whole Word Triggers
 # Problems 2-5
 
-# TODO: WordTrigger
+class WordTrigger(Trigger):
+    def __init__(self, word):
+        self.word = word
 
-# TODO: TitleTrigger
-# TODO: SubjectTrigger
-# TODO: SummaryTrigger
+    def isWordIn(self, text):
+        word = self.word.lower()
+        text = text.lower()
+        for i in string.punctuation:
+            if i in text:
+                text = text.replace(i, ' ')
+        text = text.split()
 
+        if word in text:
+            return True
+        else:
+            return False
+
+class TitleTrigger(WordTrigger):
+    def evaluate(self, story):
+        return self.isWordIn(story.getTitle())
+
+class SubjectTrigger(WordTrigger):
+    def evaluate(self, story):
+        return self.isWordIn(story.getSubject())
+
+class SummaryTrigger(WordTrigger):
+    def evaluate(self, story):
+        return self.isWordIn(story.getSummary())
 
 # Composite Triggers
 # Problems 6-8
