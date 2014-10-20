4e710ff9aa83689e33d407a07004a03269496735
prasannabe2004 <prasannabe2004@gmail.com>
2014-10-20 15:35:13 -0500

    Problem set 7 phrase trigger completed.

--- 6.00.1.x/Week7/ProblemSet/code_ProblemSet7/ps7.py
+++ 6.00.1.x/Week7/ProblemSet/code_ProblemSet7/ps7.py
@@ -114,9 +114,28 @@ class SummaryTrigger(WordTrigger):
 # Composite Triggers
 # Problems 6-8
 
-# TODO: NotTrigger
-# TODO: AndTrigger
-# TODO: OrTrigger
+class NotTrigger(Trigger):
+    def __init__(self, t1):
+        self.t1 = t1
+
+    def evaluate(self, story):
+        return not self.t1.evaluate(story)
+
+class AndTrigger(Trigger):
+    def __init__(self, t1, t2):
+        self.t1 = t1
+        self.t2 = t2
+
+    def evaluate(self, story):
+        return self.t1.evaluate(story) and self.t2.evaluate(story)
+
+class OrTrigger(Trigger):
+    def __init__(self, t1, t2):
+        self.t1 = t1
+        self.t2 = t2
+
+    def evaluate(self, story):
+        return self.t1.evaluate(story) or self.t2.evaluate(story)
 
 
 # Phrase Trigger
